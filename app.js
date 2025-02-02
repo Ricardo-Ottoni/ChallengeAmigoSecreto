5a8ff1c480e1cb4fc7bd1c25ad7580863f988f8b
Ricardo-Ottoni <ricardo.ottoni@aol.com>
2025-02-02 19:37:20 -0300

    Criação da lista de nomes de amigos. Uso de funções para criar lista, validar nome e limpar campo de entrada.

--- app.js
+++ app.js
@@ -1,3 +1,20 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
 
 let amigos = [];
+
+function adicionarAmigo() {
+    let campoNome = document.querySelector('input').value;
+
+    if (campoNome != '') {
+        amigos.push(campoNome)
+        limparCampoNome();
+        console.log(amigos);     //verificando criação da lista
+    } else {
+        alert("Por favor, insira um nome válido.")
+    }
+}
+
+function limparCampoNome() {
+    let campoNome = document.querySelector('input');
+    campoNome.value = '';
+}
